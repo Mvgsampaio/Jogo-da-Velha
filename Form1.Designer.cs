66e5574311894e99f0f2c96be9cf52f69bd497dd
Marcus Viniccius Gomes Sampaio <91703089+Mvgsampaio@users.noreply.github.com>
2021-09-30 20:59:06 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace JogodaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BT_1 = new System.Windows.Forms.Button();
+            this.BT_4 = new System.Windows.Forms.Button();
+            this.BT_7 = new System.Windows.Forms.Button();
+            this.BT_2 = new System.Windows.Forms.Button();
+            this.BT_3 = new System.Windows.Forms.Button();
+            this.BT_5 = new System.Windows.Forms.Button();
+            this.BT_8 = new System.Windows.Forms.Button();
+            this.BT_6 = new System.Windows.Forms.Button();
+            this.BT_9 = new System.Windows.Forms.Button();
+            this.Caixa_txt_titulo = new System.Windows.Forms.Label();
+            this.BT_limpa = new System.Windows.Forms.Button();
+            this.BT_fechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BT_1
+            // 
+            this.BT_1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BT_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_1.Location = new System.Drawing.Point(27, 117);
+            this.BT_1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_1.Name = "BT_1";
+            this.BT_1.Size = new System.Drawing.Size(112, 80);
+            this.BT_1.TabIndex = 0;
+            this.BT_1.UseVisualStyleBackColor = false;
+            this.BT_1.Click += new System.EventHandler(this.BT_1_Click);
+            // 
+            // BT_4
+            // 
+            this.BT_4.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BT_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_4.Location = new System.Drawing.Point(148, 117);
+            this.BT_4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_4.Name = "BT_4";
+            this.BT_4.Size = new System.Drawing.Size(112, 80);
+            this.BT_4.TabIndex = 1;
+            this.BT_4.UseVisualStyleBackColor = false;
+            this.BT_4.Click += new System.EventHandler(this.BT_1_Click);
+            // 
+            // BT_7
+            // 
+            this.BT_7.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BT_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_7.Location = new System.Drawing.Point(270, 117);
+            this.BT_7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_7.Name = "BT_7";
+            this.BT_7.Size = new System.Drawing.Size(112, 80);
+            this.BT_7.TabIndex = 2;
+            this.BT_7.UseVisualStyleBackColor = false;
+            this.BT_7.Click += new System.EventHandler(this.BT_1_Click);
+            // 
+            // BT_2
+            // 
+            this.BT_2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BT_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_2.Location = new System.Drawing.Point(27, 206);
+            this.BT_2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_2.Name = "BT_2";
+            this.BT_2.Size = new System.Drawing.Size(112, 80);
+            this.BT_2.TabIndex = 3;
+            this.BT_2.UseVisualStyleBackColor = false;
+            this.BT_2.Click += new System.EventHandler(this.BT_1_Click);
+            // 
+            // BT_3
+            // 
+            this.BT_3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BT_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_3.Location = new System.Drawing.Point(27, 295);
+            this.BT_3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_3.Name = "BT_3";
+            this.BT_3.Size = new System.Drawing.Size(112, 80);
+            this.BT_3.TabIndex = 4;
+            this.BT_3.UseVisualStyleBackColor = false;
+            this.BT_3.Click += new System.EventHandler(this.BT_1_Click);
+            // 
+            // BT_5
+            // 
+            this.BT_5.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BT_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_5.Location = new System.Drawing.Point(150, 207);
+            this.BT_5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_5.Name = "BT_5";
+            this.BT_5.Size = new System.Drawing.Size(112, 80);
+            this.BT_5.TabIndex = 5;
+            this.BT_5.UseVisualStyleBackColor = false;
+            this.BT_5.Click += new System.EventHandler(this.BT_1_Click);
+            // 
+            // BT_8
+            // 
+            this.BT_8.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BT_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_8.Location = new System.Drawing.Point(270, 206);
+            this.BT_8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_8.Name = "BT_8";
+            this.BT_8.Size = new System.Drawing.Size(112, 80);
+            this.BT_8.TabIndex = 6;
+            this.BT_8.UseVisualStyleBackColor = false;
+            this.BT_8.Click += new System.EventHandler(this.BT_1_Click);
+            // 
+            // BT_6
+            // 
+            this.BT_6.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BT_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_6.Location = new System.Drawing.Point(150, 295);
+            this.BT_6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_6.Name = "BT_6";
+            this.BT_6.Size = new System.Drawing.Size(112, 80);
+            this.BT_6.TabIndex = 7;
+            this.BT_6.UseVisualStyleBackColor = false;
+            this.BT_6.Click += new System.EventHandler(this.BT_1_Click);
+            // 
+            // BT_9
+            // 
+            this.BT_9.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BT_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_9.Location = new System.Drawing.Point(270, 295);
+            this.BT_9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_9.Name = "BT_9";
+            this.BT_9.Size = new System.Drawing.Size(112, 80);
+            this.BT_9.TabIndex = 8;
+            this.BT_9.UseVisualStyleBackColor = false;
+            this.BT_9.Click += new System.EventHandler(this.BT_1_Click);
+            // 
+            // Caixa_txt_titulo
+            // 
+            this.Caixa_txt_titulo.AutoSize = true;
+            this.Caixa_txt_titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Caixa_txt_titulo.Location = new System.Drawing.Point(98, 46);
+            this.Caixa_txt_titulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Caixa_txt_titulo.Name = "Caixa_txt_titulo";
+            this.Caixa_txt_titulo.Size = new System.Drawing.Size(223, 37);
+            this.Caixa_txt_titulo.TabIndex = 9;
+            this.Caixa_txt_titulo.Text = "Jogo da Velha";
+            // 
+            // BT_limpa
+            // 
+            this.BT_limpa.BackColor = System.Drawing.Color.Green;
+            this.BT_limpa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_limpa.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BT_limpa.Location = new System.Drawing.Point(27, 417);
+            this.BT_limpa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_limpa.Name = "BT_limpa";
+            this.BT_limpa.Size = new System.Drawing.Size(144, 46);
+            this.BT_limpa.TabIndex = 10;
+            this.BT_limpa.Text = "Limpar";
+            this.BT_limpa.UseVisualStyleBackColor = false;
+            this.BT_limpa.Click += new System.EventHandler(this.BT_inicio_Click);
+            // 
+            // BT_fechar
+            // 
+            this.BT_fechar.BackColor = System.Drawing.Color.Red;
+            this.BT_fechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_fechar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BT_fechar.Location = new System.Drawing.Point(239, 417);
+            this.BT_fechar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_fechar.Name = "BT_fechar";
+            this.BT_fechar.Size = new System.Drawing.Size(143, 46);
+            this.BT_fechar.TabIndex = 11;
+            this.BT_fechar.Text = "Fechar";
+            this.BT_fechar.UseVisualStyleBackColor = false;
+            this.BT_fechar.Click += new System.EventHandler(this.BT_fechar_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 509);
+            this.Controls.Add(this.BT_fechar);
+            this.Controls.Add(this.BT_limpa);
+            this.Controls.Add(this.Caixa_txt_titulo);
+            this.Controls.Add(this.BT_9);
+            this.Controls.Add(this.BT_6);
+            this.Controls.Add(this.BT_8);
+            this.Controls.Add(this.BT_5);
+            this.Controls.Add(this.BT_3);
+            this.Controls.Add(this.BT_2);
+            this.Controls.Add(this.BT_7);
+            this.Controls.Add(this.BT_4);
+            this.Controls.Add(this.BT_1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Jogo da Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BT_1;
+        private System.Windows.Forms.Button BT_4;
+        private System.Windows.Forms.Button BT_7;
+        private System.Windows.Forms.Button BT_2;
+        private System.Windows.Forms.Button BT_3;
+        private System.Windows.Forms.Button BT_5;
+        private System.Windows.Forms.Button BT_8;
+        private System.Windows.Forms.Button BT_6;
+        private System.Windows.Forms.Button BT_9;
+        private System.Windows.Forms.Label Caixa_txt_titulo;
+        private System.Windows.Forms.Button BT_limpa;
+        private System.Windows.Forms.Button BT_fechar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace JogodaVelha
+{
+    public partial class Form1 : Form
+    {
+
+
+        bool jogada = true;
+
+
+        private void BT_fechar_Click(object sender, EventArgs e)
+        {
+            // Ao clicar no botão fechao jogo será fechado
+            Application.Exit();
+        }
+
+        private void BT_inicio_Click(object sender, EventArgs e)
+        {
+            // limpa todos os campos
+
+            BT_1.Text = "";
+            BT_2.Text = "";
+            BT_3.Text = "";
+            BT_4.Text = "";
+            BT_5.Text = "";
+            BT_6.Text = "";
+            BT_7.Text = "";
+            BT_8.Text = "";
+            BT_9.Text = "";
+
+            //Reabilita todos os botões
+
+            BT_1.Enabled = true;
+            BT_2.Enabled = true;
+            BT_3.Enabled = true;
+            BT_4.Enabled = true;
+            BT_5.Enabled = true;
+            BT_6.Enabled = true;
+            BT_7.Enabled = true;
+            BT_8.Enabled = true;
+            BT_9.Enabled = true;
+        }
+
+        //Após leitura do botão limpa o porgrama sera reiniciado
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void BT_1_Click(object sender, EventArgs e)
+        {
+
+            Button bt = (Button)sender;
+
+            if (bt.Text == "") // se um campo estiver em branco seu proximo valor sera a sequencia da jogada de X ou O
+            {
+                // se a jogada for = a X a proxima jogada sera = a 0
+                if (jogada)
+                {
+                    bt.Text = "x";
+
+                    jogada = !jogada;
+                }
+                else
+                {
+                    bt.Text = "O";
+
+                    jogada = !jogada;
+                }                
+                   
+            }      
+        }
+    }
+}
+
